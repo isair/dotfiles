70c09f1461db65417b188209ec1946b2f9939c2d
Baris Sencan <barissncn@gmail.com>
2015-03-06 02:58:19 -0800

    Add symlink.sh
    Script for automatically creating symlinkings of dotfiles in the home folder.

---
+++ symlink.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rm ~/.profile
+ln -s profile ~/.profile
+rm ~/.bash_profile
+ln -s bash_profile ~/bash_profile
+rm ~/.zshrc
+ln -s zshrc ~/.zshrc
+rm ~/.vimrc
+ln -s vimrc ~/.vimrc
