8644cb51ef7a88a1f50fa9c20b12d787e9579d7b
Baris Sencan <baris.sncn@gmail.com>
2015-10-15 18:15:38 +0100

    Add emacs configuration

--- scripts/symlink-dotfiles.sh
+++ scripts/symlink-dotfiles.sh
@@ -8,5 +8,7 @@ rm ~/.zshrc
 ln -s $SCRIPTDIR/../zshrc ~/.zshrc
 rm ~/.vimrc
 ln -s $SCRIPTDIR/../vimrc ~/.vimrc
+rm ~/.emacs
+ln -s $SCRIPTDIR/../emacs ~/.emacs
 rm ~/.eslintrc
 ln -s $SCRIPTDIR/../eslintrc ~/.eslintrc
