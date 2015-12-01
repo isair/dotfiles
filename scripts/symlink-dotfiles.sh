a506c89247f1c89a81ad8a0db3cf50ff381bc277
Baris Sencan <baris.sncn@gmail.com>
2015-11-30 20:58:06 -0800

    [symlink] Make executable and remove obsolete lines

--- scripts/symlink-dotfiles.sh
+++ scripts/symlink-dotfiles.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 setenv SCRIPTDIR `dirname $0`
-rm ~/.profile
-ln -s $SCRIPTDIR/../profile ~/.profile
-rm ~/.bash_profile
-ln -s $SCRIPTDIR/../bash_profile ~/.bash_profile
 rm ~/.zshrc
 ln -s $SCRIPTDIR/../zshrc ~/.zshrc
 rm ~/.vimrc
