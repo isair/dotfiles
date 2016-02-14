0329e7b3239ab84e17efcad852b1924a8c1c0a64
Baris Sencan <baris.sncn@gmail.com>
2016-02-14 03:04:06 -0800

    Remove emacs configuration
    No longer using emacs.

--- scripts/symlink-dotfiles.sh
+++ scripts/symlink-dotfiles.sh
@@ -4,7 +4,5 @@ rm ~/.zshrc
 ln -s $baseDir/../zshrc ~/.zshrc
 rm ~/.vimrc
 ln -s $baseDir/../vimrc ~/.vimrc
-rm ~/.emacs
-ln -s $baseDir/../emacs ~/.emacs
 rm ~/.eslintrc
 ln -s $baseDir/../eslintrc ~/.eslintrc
