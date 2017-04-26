8524476072a3ba7581aae7e321778c97bd19ccbe
Baris Sencan <baris.sncn@gmail.com>
2017-04-25 17:08:04 -0700

    [windows] Script for streaming Twitch through VLC

---
+++ scripts/windows/Twitch.bat
@@ -0,0 +1,10 @@
+@echo off
+cd C:\Program Files (x86)\Streamlink\bin
+SET /P url=[Please enter the twitch.tv username]
+SET /P quality=[Please enter the quality]
+SET /P videoID=[Please enter the video ID or live for livestream]
+IF %videoID%==live (
+    streamlink.exe twitch.tv/%url% %quality%
+) ELSE (
+    streamlink.exe twitch.tv/%url%/v/%videoID% %quality%
+)
