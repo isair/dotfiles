c998cfdd82b9cb8de2b148d4b02f1efa2a4b6999
Baris Sencan <baris.sncn@gmail.com>
2017-08-25 02:03:02 +0100

    [windows][vm-work] Fix time interval format

--- scripts/windows/vm-work.ps1
+++ scripts/windows/vm-work.ps1
@@ -1,9 +1,9 @@
-$vboxmanage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" 
+$vboxmanage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 $params = "controlvm", "work", "keyboardputscancode", "1c"
 
 while ($true)
 {
   & $vboxmanage $params
-  $delay = Get-Random -Minimum 1 -Maximum 5
+  $delay = Get-Random -Minimum 700 -Maximum 2000
   Start-Sleep $delay
 }
