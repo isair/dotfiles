e8faf3f1e820faa1c66b49a35ef7dccfecadac07
Baris Sencan <baris.sncn@gmail.com>
2018-09-12 03:09:42 +0100

    [scripts][ubuntu] Add script for backing up nextcloud snap data

--- scripts/ubuntu/backup-nextcloud.sh
+++ scripts/ubuntu/backup-nextcloud.sh
@@ -2,15 +2,27 @@
 
 set -eu
 
+# Check if we have the necessary permissions to perform the backup.
 if [ "$(id -u)" -ne 0 ]; then
   echo "Please run backup as root."
   exit 1
 fi
 
+# Check if we have space for a backup.
+diskSize=$(df . | awk '{print $2}' | tail -n 1)
+emptySpace=$(df . | awk '{print $4}' | tail -n 1)
+spaceNeeded=$(($(du -s /var/snap/nextcloud/ | awk '{print $1}') + ${diskSize} * 2 / 10))
+if [ "${emptySpace}" -lt "${spaceNeeded}" ]; then
+  echo "You don't have enough space for a backup! You need to have at least ${spaceNeeded} kbs. You have ${emptySpace}."
+  exit 1
+fi
+
+# Export all data.
 # TODO: Modify and pass this to the export command below when we can customize the output path.
 nextcloudExportsPath="/var/snap/nextcloud/common/backups"
 
 nextcloud.export
 
+# Compress it.
 latestExportPath="${nextcloudExportsPath}/$(ls -t "${nextcloudExportsPath}" | head -1)"
 tar --remove-files -czvf "/root/nextcloud_$(date +'%Y-%m-%d_%H-%M-%S').tar.gz" "${latestExportPath}"
