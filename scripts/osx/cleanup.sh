1c8d242400a92a7a3cf7b014a1d7382fdbdea66a
Baris Sencan <baris.sncn@gmail.com>
2018-09-14 12:18:59 +0100

    [scripts][osx][install] Remove deprecated cask cleanup call

--- scripts/osx/cleanup.sh
+++ scripts/osx/cleanup.sh
@@ -4,7 +4,6 @@ rm -rf "$HOME/Library/Application Support/MobileSync/Backup"
 
 brew cleanup
 brew prune
-brew cask cleanup
 
 xcrun simctl delete unavailable
 sudo rm -rf ~/Library/Developer/Xcode/DerivedData
