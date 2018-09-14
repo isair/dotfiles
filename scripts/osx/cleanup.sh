d20e99330713fb29317740d361e8a118d4196ebe
Baris Sencan <baris.sncn@gmail.com>
2018-09-14 12:20:22 +0100

    [scripts][osx][cleanup] Attempt to clean sim files with correct perms

--- scripts/osx/cleanup.sh
+++ scripts/osx/cleanup.sh
@@ -5,5 +5,5 @@ rm -rf "$HOME/Library/Application Support/MobileSync/Backup"
 brew cleanup
 brew prune
 
-xcrun simctl delete unavailable
+sudo xcrun simctl delete unavailable
 sudo rm -rf ~/Library/Developer/Xcode/DerivedData
