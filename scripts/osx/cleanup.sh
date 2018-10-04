efc53e2f06475198ed0023f2fb22e117ddf15838
Baris Sencan <baris.sncn@gmail.com>
2018-10-04 16:48:42 +0100

    [osx][cleanup] Remove all Xcode artefacts

--- scripts/osx/cleanup.sh
+++ scripts/osx/cleanup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf "$HOME/Library/Application Support/MobileSync/Backup"
+rm -rf "${HOME}/Library/Application Support/MobileSync/Backup"
+
+rm -rf "${HOME}/Library/Developer/Xcode/Archives"
+rm -rf "${HOME}/Library/Developer/Xcode/Products"
+rm -rf "${HOME}/Library/Developer/Xcode/DerivedData"
 
 brew cleanup
 brew prune
 
 sudo xcrun simctl delete unavailable
-sudo rm -rf ~/Library/Developer/Xcode/DerivedData
