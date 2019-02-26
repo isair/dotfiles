3607efa3d848eebedb1833c19a91422f0781df5a
Baris Sencan <baris.sncn@gmail.com>
2019-02-26 05:31:52 -0500

    fix(osx-install): make zshell installation non-interrupting

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -22,8 +22,8 @@ if ! hash airport 2>/dev/null; then
 fi
 
 ## Install Oh My Zsh
-if ! hash zsh 2>/dev/null; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "${HOME}"/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 fi
 
 ## Install Homebrew
@@ -181,3 +181,6 @@ qlmanage -r
 
 ## Symlink dotfiles
 "$PWD/../unix/symlink-dotfiles.sh"
+
+## Switch shell
+chsh -s $(grep /zsh$ /etc/shells | tail -1)
