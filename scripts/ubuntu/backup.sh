cbce834354ece92cf3c3a52b90c352f019c6258a
Baris Sencan <baris.sncn@gmail.com>
2019-03-06 12:47:16 +0000

    fix(ubuntu-backup): do not list auto installed packages

--- scripts/ubuntu/backup.sh
+++ scripts/ubuntu/backup.sh
@@ -8,4 +8,5 @@ DEFAULT_BACKUP_PATH="../../data/backup"
 
 BACKUP_PATH="${1:-$DEFAULT_BACKUP_PATH}"
 
-apt list | grep \\[installed\\] | awk -F/ '{print $1}' > "${BACKUP_PATH}"/apt-packages.txt
+apt-mark showmanual | sort | grep -v -F -f <(apt show "$(apt-mark showmanual)" 2> /dev/null | grep -e ^Depends -e ^Pre-Depends | sed 's/^Depends: //; s/^Pre-Depends: //; s/(.*)//g; s/:any//g' | tr -d ',|' | tr ' ' '\n' | grep -v ^$ | sort -u) > "${BACKUP_PATH}"/apt-packages.txt
+
