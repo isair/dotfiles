a6ac02164a91a42257118453e82560258f10bbcf
Baris Sencan <baris.sncn@gmail.com>
2019-04-05 16:00:21 +0100

    feat: backup snaps

--- scripts/ubuntu/backup.sh
+++ scripts/ubuntu/backup.sh
@@ -10,3 +10,4 @@ BACKUP_PATH="${1:-$DEFAULT_BACKUP_PATH}"
 
 apt-mark showmanual | sort | grep -v -F -f <(apt show "$(apt-mark showmanual)" 2> /dev/null | grep -e ^Depends -e ^Pre-Depends | sed 's/^Depends: //; s/^Pre-Depends: //; s/(.*)//g; s/:any//g' | tr -d ',|' | tr ' ' '\n' | grep -v ^$ | sort -u) > "${BACKUP_PATH}"/apt-packages.txt
 
+snap list | awk '{if (NR > 1) print $1}' > "${BACKUP_PATH}"/snaps.txt
