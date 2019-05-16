f71bce2b835187f8e491c850b1e87de093b575dc
Baris Sencan <baris.sncn@gmail.com>
2019-05-16 09:33:06 +0100

    refactor(scripts): fix shellcheck warnings

--- scripts/unix/symlink-dotfiles.sh
+++ scripts/unix/symlink-dotfiles.sh
@@ -5,10 +5,10 @@ DEFAULT_PROFILE="personal"
 cd $(dirname "$0")/../../profiles/"${1:-$DEFAULT_PROFILE}"/configurations
 
 rm ~/.bashrc
-ln -s "$PWD/bashrc" ~/.bashrc
+ln -s "${PWD}"/bashrc ~/.bashrc
 
 rm ~/.zshrc
-ln -s "$PWD/zshrc" ~/.zshrc
+ln -s "${PWD}"/zshrc ~/.zshrc
 
 rm ~/.vimrc
-ln -s "$PWD/vimrc" ~/.vimrc
+ln -s "${PWD}"/vimrc ~/.vimrc
