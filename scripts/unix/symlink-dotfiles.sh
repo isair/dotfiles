5e5767867d212b9bd5ed8e65f0738c83960e0390
Baris Sencan <baris.sncn@gmail.com>
2021-03-24 12:59:18 +0000

    fix(scripts): do not fail if dot file does not exist

--- scripts/unix/symlink-dotfiles.sh
+++ scripts/unix/symlink-dotfiles.sh
@@ -20,26 +20,26 @@ rm -rf ~/.dotfiles-shared
 ln -s "${PWD}"/../../shared ~/.dotfiles-shared
 
 if [ -f profile ]; then
-  rm ~/.profile 2>/dev/null
+  rm -f ~/.profile
   ln -s "${PWD}"/profile ~/.profile
 fi
 
 if [ -f bashrc ]; then
-  rm ~/.bashrc 2>/dev/null
+  rm -f ~/.bashrc
   ln -s "${PWD}"/bashrc ~/.bashrc
 fi
 
 if [ -f zshrc ]; then
-  rm ~/.zshrc 2>/dev/null
+  rm -f ~/.zshrc
   ln -s "${PWD}"/zshrc ~/.zshrc
 fi
 
 if [ -f vimrc ]; then
-  rm ~/.vimrc 2>/dev/null
+  rm -f ~/.vimrc
   ln -s "${PWD}"/vimrc ~/.vimrc
 fi
 
 if [ -f hyper.js ]; then
-  rm ~/.hyper.js 2>/dev/null
+  rm -f ~/.hyper.js
   ln -s "${PWD}"/hyper.js ~/.hyper.js
 fi
